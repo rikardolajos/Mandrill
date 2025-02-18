4a0dc56c9054654eac691042f47426f947ce217a
Rikard Olajos <rikard.olajos@cs.lth.se>
2025-02-18 14:56:02 +0100

    Pass: Only allow storage image of color attachment if not using multisampling

--- src/Pass.cpp
+++ src/Pass.cpp
@@ -162,16 +162,21 @@ void Pass::createPass(bool depthAttachment, VkSampleCountFlagBits sampleCount)
     mColorAttachments.clear();
 
     for (auto format : mFormats) {
-        mColorAttachments.push_back(make_ptr<Image>(
-            mpDevice, mExtent.width, mExtent.height, 1, sampleCount, format, VK_IMAGE_TILING_OPTIMAL,
-            VK_IMAGE_USAGE_COLOR_ATTACHMENT_BIT | VK_IMAGE_USAGE_TRANSFER_SRC_BIT | VK_IMAGE_USAGE_STORAGE_BIT,
-            VK_MEMORY_PROPERTY_DEVICE_LOCAL_BIT));
+        // Color attachment can only be used as storage image if not using multisamping
+        VkImageUsageFlags usage = VK_IMAGE_USAGE_COLOR_ATTACHMENT_BIT | VK_IMAGE_USAGE_TRANSFER_SRC_BIT |
+                                  (sampleCount & VK_SAMPLE_COUNT_1_BIT ? VK_IMAGE_USAGE_STORAGE_BIT : 0);
+        mColorAttachments.push_back(make_ptr<Image>(mpDevice, mExtent.width, mExtent.height, 1, sampleCount, format,
+                                                    VK_IMAGE_TILING_OPTIMAL, usage,
+                                                    VK_MEMORY_PROPERTY_DEVICE_LOCAL_BIT));
 
         Helpers::transitionImageLayout(mpDevice, mColorAttachments.back()->getImage(), format,
                                        VK_IMAGE_LAYOUT_UNDEFINED, VK_IMAGE_LAYOUT_COLOR_ATTACHMENT_OPTIMAL, 1);
 
         mColorAttachments.back()->createImageView(VK_IMAGE_ASPECT_COLOR_BIT);
-        mColorAttachments.back()->createDescriptor();
+
+        if (sampleCount == VK_SAMPLE_COUNT_1_BIT) {
+            mColorAttachments.back()->createDescriptor();
+        }
     }
 
     if (sampleCount != VK_SAMPLE_COUNT_1_BIT) {
