85b5e0d28845b6865e105d42381b9f6c28aa64bf
Rikard Olajos <rikard.olajos@cs.lth.se>
2026-06-16 16:26:08 +0200

    Revert SPIRV-Reflect changes
    VulkanSDK only include SPIRV-Reflect on Linux, not on Windows.

--- src/Common.h
+++ src/Common.h
@@ -16,7 +16,7 @@
 #endif
 
 #include <shaderc/shaderc.hpp>
-#include <SPIRV-Reflect/spirv_reflect.h>
+#include <spirv-reflect/spirv_reflect.h>
 
 #define GLM_FORCE_RADIANS
 #define GLM_FORCE_DEPTH_ZERO_TO_ONE
